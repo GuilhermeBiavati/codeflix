e4c7595f29dddaa122ea9f53b09b2fd50951f90f
Guilherme Biavati Cagnini <guilhermebiavati@gmail.com>
2021-03-04 22:25:49 -0300

    first commit

---
+++ admin-catalog/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Models\Genre;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(CategorySeeder::class);
+        $this->call(GenreSeeder::class);
+    }
+}
